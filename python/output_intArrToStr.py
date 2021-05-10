74c57a97b92174cb3f312b9ca776977c121661e4
hamasaki ibuki <loonstap@gmail.com>
2021-05-10 12:45:03 +0900

    arrのサイズが小さい時にも対応

--- python/output_intArrToStr.py
+++ python/output_intArrToStr.py
@@ -1,5 +1,8 @@
 def output_intArrToStr(intArr):
+    if intArr == []:
+        return ""
     string = str(intArr[0])
-    for i in range(1,len(intArr)):
-        string = string + " " + str(intArr[i])
+    if len(intArr) >= 2:
+        for i in range(1,len(intArr)):
+            string = string + " " + str(intArr[i])
     return string 
